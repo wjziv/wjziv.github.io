53d1ab409a4f6c87e061cc52556cce5c000ab492
Walter Zielenski <walterzielenski@gmail.com>
2020-01-06 22:36:23 -0500

    build static pages

--- src/static/js/script.js
+++
@@ -1,9 +0,0 @@
-var $ = jQuery;
-$(document).ready(function(){
-  $("#tag-search").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#tag dt").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
